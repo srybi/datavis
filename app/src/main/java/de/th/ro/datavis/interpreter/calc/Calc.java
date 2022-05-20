ed0fd5de1bfef835b3589110672fb4d49e67115c
studstrale6953 <leopold.straub@stud.th-rosenheim.de>
2022-05-20 23:37:26 +0200

    Support various ffs files using levensthein distance to parse ffs file

--- app/src/main/java/de/th/ro/datavis/interpreter/calc/Calc.java
+++ app/src/main/java/de/th/ro/datavis/interpreter/calc/Calc.java
@@ -1,6 +1,9 @@
 package de.th.ro.datavis.interpreter.calc;
 
+import androidx.annotation.NonNull;
+
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import de.th.ro.datavis.interfaces.ICalc;
 import de.th.ro.datavis.models.FFSLine;
@@ -38,5 +41,38 @@ public class Calc {
         }else{
             return Math.log10(Math.sqrt(squaredResult));
         }
+    }
+
+    public static int calcHammingDistance(String x, String y){
+
+        int[][] dp = new int[x.length() + 1][y.length() + 1];
+
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= y.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+                else {
+                    dp[i][j] = min(dp[i - 1][j - 1]
+                                    + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
+                            dp[i - 1][j] + 1,
+                            dp[i][j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[x.length()][y.length()];
+    }
+
+    public static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    public static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                .min().orElse(Integer.MAX_VALUE);
     }
 }
