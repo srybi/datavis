2de41f9085667b41050a2c984e3f6460ab5dd3ae
studrybist5760 <stefan.rybinski@stud.th-rosenheim.de>
2022-05-26 15:32:06 +0200

    Enable basic interaction

--- app/src/main/java/de/th/ro/datavis/ui/bottomSheet/BottomSheet.java
+++ app/src/main/java/de/th/ro/datavis/ui/bottomSheet/BottomSheet.java
@@ -1,14 +1,28 @@
 package de.th.ro.datavis.ui.bottomSheet;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import de.th.ro.datavis.R;
+import de.th.ro.datavis.util.enums.InterpretationMode;
 
+/**
+ * This java class is the logical representation of modal_bottom_sheet.xml
+ */
 public class BottomSheet {
+    private String TAG = "BottomSheet";
     private Context context;
+
+    InterpretationMode mode;
 
     public BottomSheet(Context ctx){
         this.context = ctx;
@@ -19,9 +33,29 @@ public class BottomSheet {
         final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
         bottomSheetDialog.setContentView(R.layout.modal_bottom_sheet);
 
-        LinearLayout share = bottomSheetDialog.findViewById(R.id.shareLinearLayout);
-        LinearLayout download = bottomSheetDialog.findViewById(R.id.download);
+        Switch modeSwitch = bottomSheetDialog.findViewById(R.id.switchMode);
+        Button applyButton = bottomSheetDialog.findViewById(R.id.apply);
 
+        modeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    Log.d(TAG, "onCheckedChanged: checked");
+                } else {
+                    Log.d(TAG, "onCheckedChanged: uncheck");
+                }
+            }
+        });
+
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick - applyButton");
+                bottomSheetDialog.dismiss();
+            }
+        });
         bottomSheetDialog.show();
     }
+
+
 }
