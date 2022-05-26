861fea2759518b002dc587cc03614ca91dd79c3b
studrybist5760 <stefan.rybinski@stud.th-rosenheim.de>
2022-05-26 16:08:34 +0200

    Add logic + comments to bottom sheet

--- app/src/main/java/de/th/ro/datavis/ui/bottomSheet/BottomSheet.java
+++ app/src/main/java/de/th/ro/datavis/ui/bottomSheet/BottomSheet.java
@@ -19,43 +19,107 @@ import de.th.ro.datavis.util.enums.InterpretationMode;
  * This java class is the logical representation of modal_bottom_sheet.xml
  */
 public class BottomSheet {
-    private String TAG = "BottomSheet";
+    //Log Tag
+    final private String TAG = "BottomSheet";
+
+    //Context needed for init
     private Context context;
 
-    InterpretationMode mode;
+    /**
+     * List of all settings. All setting have their actual State and a changing state.
+     * - InterpretationMode
+     */
+    private InterpretationMode mode;
+    private InterpretationMode changedMode;
+
 
     public BottomSheet(Context ctx){
         this.context = ctx;
+        //default values
+        mode = InterpretationMode.Logarithmic;
     }
 
+    /**
+     * Method to open the bottom Sheet
+     */
     public void showBottomSheetDialog() {
-
         final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
         bottomSheetDialog.setContentView(R.layout.modal_bottom_sheet);
 
+        //get all interactables
         Switch modeSwitch = bottomSheetDialog.findViewById(R.id.switchMode);
         Button applyButton = bottomSheetDialog.findViewById(R.id.apply);
+        //init with current setting
+        keepSettings(bottomSheetDialog);
 
+        //handler for ModeSwitch
         modeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (isChecked) {
-                    Log.d(TAG, "onCheckedChanged: checked");
-                } else {
-                    Log.d(TAG, "onCheckedChanged: uncheck");
-                }
+                Log.d(TAG, "onCheckedChanged: handeling ModeSwitch");
+                handleModeSwitch(compoundButton, isChecked);
             }
         });
 
+        //handler for applyButton
         applyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick - applyButton");
-                bottomSheetDialog.dismiss();
+
+                if(checkChanges()){ //check if anything has changed
+                    updateSetting(); //if so update settings
+                    /*
+                    for(Subscriber s in subs){
+                        s.updated();
+                    }
+                    */
+                }
+                bottomSheetDialog.dismiss(); //close bottom sheet
             }
         });
-        bottomSheetDialog.show();
+
+        bottomSheetDialog.show(); //open bottom sheet
     }
 
+    /**
+     * This function restores the bottom sheet with its currently used settings.
+     * @param btmSheetDialog
+     */
+    private void keepSettings(BottomSheetDialog btmSheetDialog){
+        //mode switch
+        Switch modeSwitch = btmSheetDialog.findViewById(R.id.switchMode);
+        if(mode == InterpretationMode.Linear){
+            modeSwitch.setChecked(true);
+        }
+    }
+
+    /**
+     * This function checks if the bottom sheet has changed while being open.
+     * See comments on how to extend it!
+     * @return if anything has changed
+     */
+    private boolean checkChanges(){
+        boolean modeChange = changedMode != mode;
+        //boolean oderChange = changedSetting != setting;
+        return modeChange; // || oderChange;
+    }
+
+    /**
+     * This method actually updates the settings;
+     */
+    private void updateSetting(){
+        mode = changedMode;
+    }
+
+    private void handleModeSwitch(CompoundButton switchBtn, boolean isChecked){
+        if(isChecked){
+            Log.d(TAG, "handleModeSwitch: checking");
+            changedMode = InterpretationMode.Linear;
+        }else{
+            changedMode = InterpretationMode.Logarithmic;
+        }
+        switchBtn.setChecked(isChecked);
+    }
 
 }
