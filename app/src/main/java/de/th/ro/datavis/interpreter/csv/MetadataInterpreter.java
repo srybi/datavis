930aa2694be5b2f07a60b6c0e1020b9feee66a3f
studheinta5445 <tassilo.heinemann@stud.th-rosenheim.de>
2022-05-28 13:45:15 +0200

    Added Functionality to read .csv as Matrix and return MetaData objects for each line

--- app/src/main/java/de/th/ro/datavis/interpreter/csv/MetadataInterpreter.java
+++ app/src/main/java/de/th/ro/datavis/interpreter/csv/MetadataInterpreter.java
@@ -10,22 +10,31 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import de.th.ro.datavis.interpreter.calc.Calc;
-import de.th.ro.datavis.interpreter.ffs.FFSConstants;
-import de.th.ro.datavis.models.Sphere;
-import de.th.ro.datavis.util.enums.InterpretationMode;
+
+import de.th.ro.datavis.models.Antenna;
+import de.th.ro.datavis.models.MetaData;
 import de.th.ro.datavis.util.enums.MetadataType;
 import de.th.ro.datavis.util.exceptions.CSVException;
-import de.th.ro.datavis.util.exceptions.FFSInterpretException;
 
 public class MetadataInterpreter {
 
+    private static final String LOG_TAG = "MetaIntrp";
+
     public MetadataInterpreter() {}
 
+    /**
+     *
+     * @param in InputStream from file
+     * @return  Matrix of .csv Data
+     * @throws CSVException
+     */
     public String[][] interpretCSV(InputStream in) throws CSVException {
         InputStreamReader reader = new InputStreamReader(in);
         BufferedReader br = new BufferedReader(reader);
@@ -48,6 +57,39 @@ public class MetadataInterpreter {
             matrix[i] = row;
         }
         return matrix;
+    }
+
+    /**
+     *
+     * @param in - Input Stream
+     * @return Simple List of lines of the .csv Data
+     */
+    public List<String> interpretMetaData(InputStream in) throws IOException {
+        InputStreamReader reader = new InputStreamReader(in);
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))) {
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e){}
+        return lines;
+    }
+
+    /**
+     * @param matrix - Takes .csv matrix
+     * @return - Gives out Metadata information, including tilt etc.
+     * Excluding Filename, AntennaID
+     */
+    public List<MetaData> getMetadataFromLines(String[][] matrix) {
+        List<MetaData> mList = new ArrayList<>();
+        for(int i=1; i<matrix.length; i++) {
+            for(int j=1; j<matrix[i].length; j++) {
+                MetaData m = new MetaData(matrix[i][0],matrix[0][j],matrix[i][j]);
+                mList.add(m);
+            }
+        }
+        return mList;
     }
 
 }
