9529dd5f5f5a4035b287e2ebf2f0049a21730de5
studheinta5445 <tassilo.heinemann@stud.th-rosenheim.de>
2022-06-09 20:55:03 +0200

    Added Filechecks für Antenne, FFS und Metadata in FileHandler - Nach File Extension und Mime type.

--- app/src/main/java/de/th/ro/datavis/util/constants/FileRequests.java
+++ app/src/main/java/de/th/ro/datavis/util/constants/FileRequests.java
@@ -2,9 +2,9 @@ package de.th.ro.datavis.util.constants;
 
 public class FileRequests {
 
-    public static int REQUEST_CODE_ANTENNA = 666;
-    public static int REQUEST_CODE_FFS = 555;
-    public static int REQUEST_CODE_METADATA = 444;
-    public static int REQUEST_CODE_METADATAFOLDER = 488;
+    public static final int REQUEST_CODE_ANTENNA = 666;
+    public static final int REQUEST_CODE_FFS = 555;
+    public static final int REQUEST_CODE_METADATA = 444;
+    public static final int REQUEST_CODE_METADATAFOLDER = 488;
 
 }
