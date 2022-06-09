8703eaa891a79861a9c0ea0ea93e1a3cfef37f30
studkernsa1899 <samuel.kern@stud.th-rosenheim.de>
2022-06-09 22:29:29 +0200

    About Page added

---
+++ app/src/main/java/de/th/ro/datavis/about/AboutFragment.java
@@ -0,0 +1,11 @@
+package de.th.ro.datavis.about;
+
+import de.th.ro.datavis.R;
+import de.th.ro.datavis.util.fragment.BaseFragment;
+
+public class AboutFragment extends BaseFragment {
+    @Override
+    public int registerLayoutResource() {
+        return R.layout.fragment_about;
+    }
+}
