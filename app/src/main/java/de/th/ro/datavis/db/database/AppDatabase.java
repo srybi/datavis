a1b9b0a1d963492553ba1d315f6caf8191fc2d93
studheinta5445 <tassilo.heinemann@stud.th-rosenheim.de>
2022-06-10 20:17:21 +0200

    Changed Metadata Tilt to double; removed some obsolete code

--- app/src/main/java/de/th/ro/datavis/db/database/AppDatabase.java
+++ app/src/main/java/de/th/ro/datavis/db/database/AppDatabase.java
@@ -18,7 +18,7 @@ import de.th.ro.datavis.models.AtomicField;
 import de.th.ro.datavis.models.MetaData;
 
 @TypeConverters({Converter.class})
-@Database(entities = {AntennaField.class, Antenna.class, MetaData.class, AtomicField.class}, version = 15)
+@Database(entities = {AntennaField.class, Antenna.class, MetaData.class, AtomicField.class}, version = 12)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract AntennaFieldDao antennaFieldDao();
